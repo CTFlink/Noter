51414e24ca70a12ef2dec78528f8027571e60147
Christian T Flink <chri62aa@edu.easj.dk>
2018-09-20 08:49:41 +0200

    first

---
+++ src/Noter.java
@@ -0,0 +1,23 @@
+public class Noter {
+
+
+    public static void main(String[] args) {
+        //System er class , out er objectet og println er subroutine
+
+        System.out.println("Hello World!");
+        //Her bliver forskellige variable både declared, og derefter assigned. Dvs. de bliver initialized
+
+        byte Kortest = 1;         //1 byte
+        short Kort = 2;           //2 bytes
+        int Mellem = 4;           //4 bytes
+        long Lang = 8;            //8 bytes
+
+        float KortKomma = 1.234f; //4 bytes float number
+        double LangKomma = 1.23;  //8 bytes float number (double er oftest brugt)
+
+        char Tegn = '&';          //2 bytes kun ét enkelt bogstav, tegn eller tal
+        boolean Sandt = true;    //1 bytes sandt eller falsk
+
+
+    }
+}
